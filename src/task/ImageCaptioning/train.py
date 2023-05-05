1849418ee9bc714a90f1c27c7a372955b41b8dae
Mahsa <khoshnoodi.ma@gmail.com>
2023-05-05 23:44:55 +0330

    Add ImageCaptioning task

---
+++ src/task/ImageCaptioning/train.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transform as transforms
+from torch.utils.tensorboard import SummaryWriter
+from utils import save_checkpoint, load_checkpoints, print_examples
+from loader import get_loader
+from model import CNNtoRNN
+
+def train():
+    transforms = transforms.Compose(
+        [
+            transforms.Resize((356, 356)),
+            transforms.RandomCrop((299, 299)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]
+    )
+
+    train_loader, dataset = get_loader(
+        root_floder='flicker8k/images',
+        annotation_files='flicker8k/captions.txt',
+        transforms=transforms,
+        num_workers=2
+    )
+
+    torch.backends.cudnn.benchmark = True
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    load_model = False
+    save_model = True
+
+    embed_size = 256
+    hidden_size = 256
+    vocab_size = len(dataset.vocab)
+    num_layers = 1
+    learning_rate = 3e-4
+    num_epochs = 100
+
+    writer = SummaryWriter("run/flicker")
+    step = 0
+
+    model = CNNtoRNN(embed_size, hidden_size, vocab_size, num_layers).to(device)
+    loss = nn.CrossEntropyLoss(ignore_index=dataset.vocab.stoi["PAD"])
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    if load_model:
+        step = load_checkpoints(torch.load("my_checkpoint.pth.tar"), model, optimizer)
+
+    model.train()
+
+    for epoch in range(num_epochs):
+        print_examples(model, device,dataset)
+        if save_model:
+            checkpoint = {
+                "state_dict" : model.state_dict(),
+                "optimizer" : optimizer.state_dict(),
+                "step" : step
+            }
+            save_checkpoint(checkpoint)
+
+        for idx, (imgs, captions) in enumerate(train_loader):
+            imgs = imgs.to(device)
+            captions = captions.to(device)
+
+            outputs = model(imgs, captions[:-1])
+            loss = loss(outputs.reshape(-1, outputs.shape[2]), captions.reshape(-1))
+
+            writer.add_scalar("Training loss", loss.item(), global_step=step)
+            step += 1
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+if __name__ == "__main__":
+    train()
+
