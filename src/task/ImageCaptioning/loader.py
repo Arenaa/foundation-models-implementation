735ac3b42fd68db5b2c8236e011b54f38b801009
Mahsa <khoshnoodi.ma@gmail.com>
2023-05-07 23:46:02 +0330

    change loader module

--- src/task/ImageCaptioning/loader.py
+++ src/task/ImageCaptioning/loader.py
@@ -1,26 +1,97 @@
 import os
 import pandas as pd
-import spacy
+from spacy import *
 import torch
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 
-class get_loader(Dataset):
-    def __init__(self, csv_file, root_dir, transform=None):
-        self.annotations = pd.read_csv(csv_file)
+class Vocabulary:
+    def __init__(self, freq_threshold):
+        self.itos = {0:"<PAD>", 1:"<SOS>", 2:"<EOS>", 3:"<UNK>"}
+        self.stoi = {"<PAD>":0, "<SOS>":1, "<EOS>":2, "<UNK>":3}
+        self.freq_threshold = freq_threshold
+
+    def __len__(self):
+        return len(self.itos)
+
+    @staticmethod
+    def tokenizer_eng(text):
+        return [tok.text.lower() for tok in spacy_eng.tokenizer(text)]
+
+    def build_vocabulary(self, sentence_list):
+        frequencies = {}
+        idx = 4
+
+        for sentence in sentence_list:
+            for word in self.tokenizer_eng(sentence):
+                if word not in frequencies:
+                    frequencies[word] = 1
+                else:
+                    frequencies[word] += 1
+
+                if frequencies[word] == self.freq_threshold:
+                    self.stoi[word] = idx
+                    self.itos[idx] = word
+                    idx += 1
+
+    def numericalize(self, text):
+        tokenizered_text = self.tokenizer_eng(text)
+
+        return [
+            self.stoi[token] if token in self.stoi else self.stoi["<UNK>"]
+            for token in tokenizered_text
+        ]
+
+
+class FlickerDataset(Dataset):
+    def __init__(self, root_dir, caption_file, transform=None, freq_threshold=5):
+        self.df = pd.read_csv(caption_file)
         self.root_dir = root_dir
         self.transform = transform
 
+        self.img = self.df["image"]
+        self.caption = self.df["caption"]
+
+        self.vocab = Vocabulary(freq_threshold)
+        self.vocab.build_vocabulary(self.caption.tolist())
+
     def __len__(self):
-        return len(self.annotations)
+        return len(self.df)
 
     def __getitem__(self, index):
-        img_path = os.path.join(self.root_dir, self.annotations.iloc[index, 0])
-        image = io.imread(img_path)
-        y_label = torch.tensor(int(self.annotations.iloc[index, 1]))
+        caption = self.caption[index]
+        img_id = self.img[index]
+        img = Image.open(os.path.join(self.root_dir, img_id)).convert("RGB")
+
 
         if self.transform:
-            image = self.transform(image)
+            img = self.transform(img)
 
-            return (image, y_label)
+        numericalized_caption = [self.vocab.stoi["<SOS>"]]
+        numericalized_caption += self.vocab.numericalize(caption)
+        numericalized_caption.append(self.vocab.stoi["<EOS>"])
+
+        return img, torch.Tensor(numericalized_caption)
+
+class MyCollate:
+    def __init__(self, pad_idx):
+        self.pad_idx = pad_idx
+
+    def __call__(self, batch):
+        imgs = [item[0].unsqueeze(0) for item in batch]
+        imgs = torch.cat(imgs, dim=0)
+        targets = [item[1] for item in batch]
+        targets = pad_sequence(targets, batch_first=False, padding_value=self.pad_idx)
+
+        return imgs, targets
+
+def get_loader(
+        root_folder,
+        annotation_file,
+        transform,
+        batch_size=32,
+        num_worker=8,
+        shuffle=True
+):
+    pass
