431e77076c92779d98088c0318f1d5c152840776
Mahsa <khoshnoodi.ma@gmail.com>
2023-05-14 22:05:29 +0330

    fix a bug

--- src/task/ImageSegmentation/model.py
+++ src/task/ImageSegmentation/model.py
@@ -56,7 +56,7 @@ class UNET(nn.Module):
 
             if x.shape != skip_connection.shape:
                 x = TF.resize(x, size=skip_connection.shape[2:], antialias=True)
-            
+
             concat_skip = torch.cat((skip_connection, x), dim=1)
             x = self.ups[idx+1](concat_skip)
 
