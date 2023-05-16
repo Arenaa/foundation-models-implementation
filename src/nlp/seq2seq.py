32ffa22ae0a10d1699eaa878262b4a8500405202
Mahsa <khoshnoodi.ma@gmail.com>
2023-05-16 23:10:18 +0330

    fix some bugs

--- src/nlp/seq2seq.py
+++ src/nlp/seq2seq.py
@@ -8,8 +8,11 @@ import spacy
 import random
 from torch.utils.tensorboard import SummaryWriter
 
-spacy_ger = spacy.load('de')
-spacy_eng = spacy.load('en')
+# Download these spacy packages
+# python -m spacy download de_core_news_sm
+# python -m spacy download en_core_web_sm
+spacy_ger = spacy.load('de_core_news_sm')
+spacy_eng = spacy.load('en_core_web_sm')
 
 def toeknizer_ger(text):
     return [tok.text for tok in spacy_ger.tokenizer(text)]
@@ -23,7 +26,8 @@ german = Field(tokenize=toeknizer_ger, lower=True,
 english = Field(tokenize=toeknizer_eng, lower=True,
                init_token='<sos>', eos_token='<eos>')
 
-train_data, validation_data, test_data = Multi30k.split(exts=('.de' , '.en'),
+train_data, validation_data, test_data = Multi30k.splits(exts=('.de' ,
+                                                               '.en'),
                                                         fields=(german, english))
 
 german.build_vocab(train_data, max_size=10000, min_freq=2)
@@ -88,62 +92,62 @@ class Seq2seq(nn.Module):
 
         return outputs
 
-    num_epochs = 20
-    learning_rate = 0.001
-    batch_size = 64
-    load_model = False
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    input_size_encoder = len(german.vocab)
-    input_size_decoder = len(english.vocab)
-    output_size = len(english.vocab)
-    encodr_embedding = 300
-    decoder_embedding = 300
-    hidden_size = 1024
-    num_layers = 2
-    enc_dropout = 0.5
-    dec_dropout = 0.5
+num_epochs = 20
+learning_rate = 0.001
+batch_size = 64
+load_model = False
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+input_size_encoder = len(german.vocab)
+input_size_decoder = len(english.vocab)
+output_size = len(english.vocab)
+encodr_embedding = 300
+decoder_embedding = 300
+hidden_size = 1024
+num_layers = 2
+enc_dropout = 0.5
+dec_dropout = 0.5
 
-    writer = SummaryWriter(f'runs/loss_plot')
-    step = 0
+writer = SummaryWriter(f'runs/loss_plot')
+step = 0
 
-    train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
-        (train_data, validation_data, test_data),
-        batch_size,
-        sort_within_batch = True,
-        sort_key = lambda x:len(x.src),
-        device=device
-    )
+train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
+    (train_data, validation_data, test_data),
+    batch_size,
+    sort_within_batch = True,
+    sort_key = lambda x:len(x.src),
+    device=device
+)
 
-    encoder_net = Encoder(input_size_encoder, encodr_embedding, hidden_size, num_layers, enc_dropout).to(device)
+encoder_net = Encoder(input_size_encoder, encodr_embedding, hidden_size, num_layers, enc_dropout).to(device)
 
-    decoder_net = Decoder(input_size_decoder, decoder_embedding, hidden_size, num_layers, dec_dropout).to(device)
+decoder_net = Decoder(input_size_decoder, decoder_embedding, hidden_size, num_layers, dec_dropout).to(device)
 
-    model = Seq2seq(encoder_net, decoder_net).to(device)
-    optimizer = optim.Adam(model.parameters(), learning_rate=learning_rate)
+model = Seq2seq(encoder_net, decoder_net).to(device)
+optimizer = optim.Adam(model.parameters(), learning_rate=learning_rate)
 
-    pad_idx = english.vocab.stoi['<pad>']
-    loss_fn = nn.CrossEntropyLoss(ignore_index=pad_idx)
+pad_idx = english.vocab.stoi['<pad>']
+loss_fn = nn.CrossEntropyLoss(ignore_index=pad_idx)
 
-    for epoch in range(num_epochs):
-        print(f'Epoch [{epoch} / {num_epochs}]')
+for epoch in range(num_epochs):
+    print(f'Epoch [{epoch} / {num_epochs}]')
 
-        for batch_idx, batch in enumerate(train_iterator):
-            inp_data = batch.src.to(device)
-            target = batch.targ.to(device)
+    for batch_idx, batch in enumerate(train_iterator):
+        inp_data = batch.src.to(device)
+        target = batch.targ.to(device)
 
-            output = model(inp_data, target)
+        output = model(inp_data, target)
 
-            output = output[1:].rehspae(-1, output.shape[2])
-            target = target[1:].reshape(-1)
+        output = output[1:].rehspae(-1, output.shape[2])
+        target = target[1:].reshape(-1)
 
-            optimizer.zero_grad()
-            loss = loss_fn(output, target)
-            loss.backward()
+        optimizer.zero_grad()
+        loss = loss_fn(output, target)
+        loss.backward()
 
-            torch.nn.utils.clip_grad(model.parameters(), max_norm=1)
-            optimizer.step()
+        torch.nn.utils.clip_grad(model.parameters(), max_norm=1)
+        optimizer.step()
 
-            writer.add_scalar('Translating loss', loss, global_step=step)
-            step +=1
+        writer.add_scalar('Translating loss', loss, global_step=step)
+        step +=1
 
 
